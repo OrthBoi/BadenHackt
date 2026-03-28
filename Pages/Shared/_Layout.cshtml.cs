fef57acee5b26f3a2c3b6fe764b89600aa133efd
prolol23-debug <L.Wilcox.inf24@stud.bbbaden.ch>
2026-03-28 08:59:00 +0100

    TrackmyFood

---
+++ Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TrackmyFood.Pages.Shared
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+                                           
+        }
+    }
+}
